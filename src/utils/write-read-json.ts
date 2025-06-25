785a6ba93e1bd8db59d99a9a84b5705059c6aa26
eduardo-nicola <eduardonicolagit@gmail.com>
2025-06-25 20:34:45 -0300

    chore: not log saved json

--- src/utils/write-read-json.ts
+++ src/utils/write-read-json.ts
@@ -13,5 +13,4 @@ export function readJsonFile(): PathEntry[] {
 
 export function writeToJsonFile(data: PathEntry[]): void {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf-8");
-  console.log("Path added successfully!");
 }
