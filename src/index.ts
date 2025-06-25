8ee6054e1933564177f24697121ec6cd49eb8b26
eduardo-nicola <eduardonicolagit@gmail.com>
2025-06-25 20:33:55 -0300

    doc: lib description cli

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,14 @@ import { editPath } from './commands/edit-path';
 const program = new Command();
 
 program
+  .name('pf')
+  .description('A CLI tool to manage project paths with shortcuts')
+  .version('1.0.0'); // Check
+
+program
   .command('add <path> <command>')
   .description('Add a project path with a shortcut')
-  .action(addPath);
+  .action(addPath); // Check
 
 program
   .command('go <command>')
@@ -20,15 +25,15 @@ program
   .description('Navigate to the project path, open it in VS Code, and run your custom commands.')
   .action(goPath);
 
-  program
+program
   .command('list')
   .description('List all registered paths')
-  .action(listPaths);
+  .action(listPaths); // Check
 
 program
   .command('delete <command>')
   .description('Delete a path by its command (shortcut)')
-  .action(deletePath);
+  .action(deletePath); // Check
 
 program
   .command('edit <command>')
